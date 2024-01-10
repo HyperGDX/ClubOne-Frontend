6961022e8c5b6fe520291012cba2738a73ef621f
HyperGDX <73820198+HyperGDX@users.noreply.github.com>
2024-01-10 13:38:15 +0800

    Update route (#4)
    * [Feature] Add router and static whatsnew
    
    * [Fix] Route change
    
    * [Fix] Update route

---
+++ src/pages/Login/index.vue
@@ -0,0 +1,8 @@
+<template>
+    Login Page
+    <router-link to="/WhatsNew">
+        <el-button type="primary" size="large">登陆</el-button>
+    </router-link>
+</template>
+<script setup lang="ts">
+</script>
