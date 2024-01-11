611ac02204349b09f623c72d092132df623acffa
yue_yu <yue_yu@trendmicro.com>
2024-01-11 15:49:06 +0800

    [feat]: forum general initial

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,30 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
-import Login from '../pages/Login/index.vue';
-import Menu from '../pages/Menu/index.vue'
-import WhatsNew from '../pages/WhatsNew/index.vue';
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import Login from "../pages/Login/index.vue";
+import Menu from "../pages/Menu/index.vue";
+import WhatsNew from "../pages/WhatsNew/index.vue";
+import General from "../pages/Forum/General/index.vue";
 
 const routes: RouteRecordRaw[] = [
-    { path: '/', redirect: '/WhatsNew', },
-    { path: '/', name: 'Menu', component: Menu,
-        children: [
-            { path: 'WhatsNew', component: WhatsNew, },
-        ],
-    },
-    { path: "/Login", name: 'Login', component: Login, },
+  { path: "/", redirect: "/WhatsNew" },
+  {
+    path: "/",
+    name: "Menu",
+    component: Menu,
+    children: [
+      { path: "WhatsNew", component: WhatsNew },
+      { path: "forum", children: [{ path: "general", component: General }] },
+    ],
+  },
+  { path: "/Login", name: "Login", component: Login },
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes,
+  history: createWebHashHistory(),
+  routes,
 });
 
+// export const allRoutes = {
+//   forumGeneral: "/forum/general",
+// };
 
 export default router;
