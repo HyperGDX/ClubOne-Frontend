44706058c019bb2b868017e539bf58b62663e297
HyperGDX <1050264043@qq.com>
2024-01-16 15:28:47 +0800

    [Feature] Show three kinds cards

--- src/pages/WhatsNew/whatsNewIndex.vue
+++ src/pages/WhatsNew/whatsNewIndex.vue
@@ -5,9 +5,64 @@
         <el-button type="primary">Refresh</el-button>
       </el-header>
       <el-main>
+        <p>Forum</p>
         <div class="grid-container">
           <el-card
-            v-for="(card, index) in cards"
+            v-for="(card, index) in forumCards"
+            :key="index"
+            class="grid-item"
+          >
+            <div class="image-container">
+              <img :src="card.image" alt="card image" class="card-image" />
+            </div>
+            <div class="card-content">
+              <div class="creator-info">
+                <img
+                  :src="card.avatar"
+                  alt="creator avatar"
+                  class="creator-avatar"
+                />
+                <p>{{ card.creator }}</p>
+                <p class="creator-time">{{ card.createdTime }}</p>
+              </div>
+              <div class="likes-info">
+                <p class="likes-count">{{ card.likeCount }}</p>
+              </div>
+            </div>
+          </el-card>
+        </div>
+
+        <p>Club</p>
+        <div class="grid-container">
+          <el-card
+            v-for="(card, index) in clubCards"
+            :key="index"
+            class="grid-item"
+          >
+            <div class="image-container">
+              <img :src="card.image" alt="card image" class="card-image" />
+            </div>
+            <div class="card-content">
+              <div class="creator-info">
+                <img
+                  :src="card.avatar"
+                  alt="creator avatar"
+                  class="creator-avatar"
+                />
+                <p>{{ card.creator }}</p>
+                <p class="creator-time">{{ card.createdTime }}</p>
+              </div>
+              <div class="likes-info">
+                <p class="likes-count">{{ card.likeCount }}</p>
+              </div>
+            </div>
+          </el-card>
+        </div>
+
+        <p>EShop</p>
+        <div class="grid-container">
+          <el-card
+            v-for="(card, index) in shopCards"
             :key="index"
             class="grid-item"
           >
@@ -36,31 +91,30 @@
 </template>
 
 <script lang="ts">
-import { reactive, onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import getLastestCards from '@/api/whatsnew.ts';
 
-const cardNum = 2;
+const cardNum = 3;
 
 export default {
   setup() {
-    const state = reactive<{
-      cards: any[];
-      error: Error | null;
-    }>({
-      cards: [],
-      error: null,
-    });
+    const forumCards = ref([]);
+    const clubCards = ref([]);
+    const shopCards = ref([]);
 
     onMounted(async () => {
       try {
         const response = await getLastestCards(cardNum);
-        state.cards = response.data.cards;
+        forumCards.value = response.data.forumCards.slice(0, cardNum);
+        clubCards.value = response.data.clubCards.slice(0, cardNum);
+        shopCards.value = response.data.shopCards.slice(0, cardNum);
       } catch (error) {
-        state.error = error as Error;
+        // eslint-disable-next-line no-console
+        console.error(error);
       }
     });
 
-    return state;
+    return { forumCards, clubCards, shopCards };
   },
 };
 </script>
@@ -74,6 +128,7 @@ export default {
 
 .grid-item {
   width: 100%;
+  min-height: 100px;
 }
 
 .image-container {
