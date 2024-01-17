b00618c71fe4ab7d78c5fe567c23476d0487d2aa
TobeNext <1145252363@qq.com>
2024-01-17 09:55:23 +0800

    fix(api): Change params way

--- src/api/forums.ts
+++ src/api/forums.ts
@@ -2,7 +2,8 @@ import request from '@/utils/request.ts';
 
 export default function getPosts(channelId: number, pageIndex: number) {
   return request({
-    url: `/posts?channelId=${channelId}&pageIndex=${pageIndex}`,
+    url: `/posts`,
     method: 'get',
+    params: { channelId, pageIndex },
   });
 }
