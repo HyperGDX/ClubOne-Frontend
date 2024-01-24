55d647d9986e26e5f7a37b9e8918371969cb6212
TobeNext <1145252363@qq.com>
2024-01-24 17:46:29 +0800

    [fix]: general lazy load bugfix

--- src/pages/Forum/General/forumGeneral.vue
+++ src/pages/Forum/General/forumGeneral.vue
@@ -2,6 +2,7 @@
   <div
     id="app"
     v-infinite-scroll="handleScroll"
+    v-infinite-scroll-disabled="disabled"
     style="overflow: auto; height: 100vh"
   >
     <div class="post" v-for="(post, index) in posts" :key="index">
@@ -63,165 +64,50 @@
       </el-container>
     </div>
     <div v-if="loading">Loading...</div>
+    <div v-if="noMore">No more</div>
   </div>
 </template>
 
-<!-- <script lang="ts">
-export default {
-  data() {
-    return {
-      posts: [
-        {
-          image:
-            'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-          creator: 'Creator1',
-          avatar:
-            'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-          createdAt: '2022-01-01',
-          likes: 10,
-          views: 10,
-          content: 'content1',
-          title: 'Title1',
-          pics: [
-            {
-              url: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-            },
-            {
-              url: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-            },
-          ],
-        },
-        {
-          image:
-            'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-          creator: 'Creator2',
-          avatar:
-            'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-          createdAt: '2022-01-02',
-          likes: 20,
-          views: 10,
-          content: 'content1',
-          title: 'Title2',
-          pics: [
-            {
-              url: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-            },
-            {
-              url: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-            },
-          ],
-        },
-        {
-          image:
-            'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-          creator: 'Creator3',
-          avatar:
-            'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-          createdAt: '2022-01-03',
-          likes: 30,
-          views: 10,
-          content: 'content1',
-          title: 'Title3',
-          pics: [
-            {
-              url: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-            },
-            {
-              url: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-            },
-          ],
-        },
-        {
-          image:
-            'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-          creator: 'Creator4',
-          avatar:
-            'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-          createdAt: '2022-01-04',
-          likes: 40,
-          views: 10,
-          content: 'content1',
-          title: 'Title4',
-          pics: [
-            {
-              url: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-            },
-            {
-              url: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-            },
-          ],
-        },
-        {
-          image:
-            'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-          creator: 'Creator5',
-          avatar:
-            'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-          createdAt: '2022-01-05',
-          likes: 50,
-          views: 10,
-          content: 'content1',
-          title: 'Title5',
-          pics: [
-            {
-              url: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-            },
-            {
-              url: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-            },
-          ],
-        },
-        {
-          image:
-            'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-          creator: 'Creator6',
-          avatar:
-            'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-          createdAt: '2022-01-06',
-          likes: 60,
-          views: 10,
-          content: 'content1',
-          title: 'Title6',
-          pics: [
-            {
-              url: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-            },
-            {
-              url: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-            },
-          ],
-        },
-      ],
-    };
-  },
-};
-</script> -->
-
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import getPosts from '@/api/forums.ts';
 import type { Posts } from '@/types/forum.d.ts';
 
 const channelId = 0; // 请替换为你的channelId
-let pageIndex = 1;
+const pageIndex = ref(0);
 const posts = ref<Posts[]>([]);
 const loading = ref(false);
 
 const fetchPosts = async () => {
   loading.value = true;
-  const res = (await getPosts(channelId, pageIndex)).data;
-  // console.log(response);
-  posts.value = [...posts.value, ...res.data];
-  loading.value = false;
-  pageIndex += 1;
-};
-
-const handleScroll = () => {
-  fetchPosts();
+  // 设置滚动节流时间
+  setTimeout(async () => {
+    const res = (await getPosts(channelId, pageIndex.value)).data;
+    posts.value = [...posts.value, ...res.data];
+    loading.value = false;
+  }, 1000);
 };
 
 onMounted(() => {
   fetchPosts();
+});
+
+const handleScroll = computed(() => {
+  return () => {
+    if (loading.value) {
+      return;
+    }
+    pageIndex.value += 1;
+    fetchPosts();
+  };
+});
+
+const noMore = computed(() => {
+  return pageIndex.value > 5;
+});
+
+const disabled = computed(() => {
+  return loading.value || noMore.value;
 });
 </script>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import Login from '../pages/Login/loginIndex.vue';
 import Menu from '../pages/Menu/menuIndex.vue';
 import WhatsNew from '../pages/WhatsNew/whatsNewIndex.vue';
-import General from '../pages/Forum/General/forumGeneral.vue';
 
 const routes: RouteRecordRaw[] = [
   { path: '/', redirect: '/whatsNew' },
@@ -12,7 +11,16 @@ const routes: RouteRecordRaw[] = [
     component: Menu,
     children: [
       { path: 'whatsNew', component: WhatsNew },
-      { path: 'forum', children: [{ path: 'general', component: General }] },
+      {
+        path: 'forum',
+        children: [
+          {
+            path: 'general',
+            // 实现路由懒加载
+            component: () => import('../pages/Forum/General/forumGeneral.vue'),
+          },
+        ],
+      },
     ],
   },
   { path: '/login', name: 'Login', component: Login },
