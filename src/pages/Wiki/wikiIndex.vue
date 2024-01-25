6448a15635fa949674ef443e4a65507fc46328ef
HyperGDX <1050264043@qq.com>
2024-01-25 13:41:57 +0800

    [Feature] Change textarea to quill

--- src/pages/Wiki/wikiIndex.vue
+++ src/pages/Wiki/wikiIndex.vue
@@ -1,40 +1,51 @@
 <template>
-  <el-input
-    v-model="textareaValue"
-    type="textarea"
-    placeholder="Please input"
+  <quill-editor
+    ref="QuillEditor"
+    v-model:content="wikiContent"
+    contentType="html"
   />
+  <button v-on:click="printContent">Print Content In Console</button>
 </template>
 
 <script lang="ts">
 import { ref, watch, onMounted } from 'vue';
 import { throttle } from 'lodash';
 import { updateWiki, getWiki } from '@/api/wiki.ts';
+import { QuillEditor } from '@vueup/vue-quill';
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
 
 const wikiIndex = 'aabbcc';
 
 export default {
+  components: {
+    QuillEditor,
+  },
+  methods: {
+    printContent() {
+      // eslint-disable-next-line
+      console.log(this.wikiContent);
+    },
+  },
   setup() {
-    const textareaValue = ref('');
-    const throttleUpdateAPI = throttle(updateWiki, 1000);
-
+    const wikiContent = ref('');
+    const throttleUpdateAPI = throttle(updateWiki, 3000);
     watch(
-      textareaValue,
+      wikiContent,
       (_, oldVal) => {
         if (oldVal !== '') {
-          throttleUpdateAPI(wikiIndex, textareaValue.value);
+          throttleUpdateAPI(wikiIndex, wikiContent.value);
         }
       },
       { flush: 'post' }
     );
     onMounted(async () => {
       getWiki(wikiIndex).then((response) => {
-        textareaValue.value = response.data.data.wikiContent;
+        wikiContent.value = response.data.data.wikiContent;
       });
     });
 
     return {
-      textareaValue,
+      wikiContent,
     };
   },
 };
