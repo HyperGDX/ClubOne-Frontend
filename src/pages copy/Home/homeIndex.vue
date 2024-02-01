e47f70840fe55ba3206a7df88eded2f5a0817b73
TobeNext <1145252363@qq.com>
2024-02-01 14:27:45 +0800

    fix: del pages du

--- src/pages copy/Home/homeIndex.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  Home
-  <router-link to="whatsNew">
-    <el-button type="primary" size="large">登陆</el-button>
-  </router-link>
-</template>
-<script setup lang="ts"></script>
--- src/pages copy/pages/Login/loginIndex.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  Login Page
-  <router-link to="/whatsNew">
-    <el-button type="primary" size="large">登陆</el-button>
-  </router-link>
-</template>
-<script setup lang="ts"></script>
